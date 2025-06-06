aaa78397de4387489961470556235b3b968a6271
parathabanado <parathacodes@gmail.com>
2025-06-06 14:32:28 +0000

    late commit

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x,y,PLAYER_RADIUS)
+
+    def draw(self,screen):
+        pygame.draw.circle(screen, "grey", self.position ,PLAYER_RADIUS,2)
+    
+    def update(self,dt):
+        self.position+=self.velocity*dt
+        
--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import pygame
+from player import Player
+from pygame.locals import *
 from constants import *
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock=pygame.time.Clock()
+    dt=0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers=(updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable,drawable)
+    player=Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    field=AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((1,1,1))
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collision_check(asteroid) == True :
+                print("Game over!")
+                return
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        dt=(clock.tick(60))/1000
 if __name__ == "__main__":
     main()
